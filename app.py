9103985241b34da5a8ae94e1ec3c37ff941e91b9
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2022-11-17 22:15:51 -0500

    added profile editor

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UpdateUserForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -209,11 +209,32 @@ def stop_following(follow_id):
     return redirect(f"/users/{g.user.id}/following")
 
 
-@app.route('/users/profile', methods=["GET", "POST"])
-def profile():
+@app.route('/users/profile/<int:user_id>', methods=["GET", "POST"])
+def profile(user_id):
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    user = User.query.get_or_404(user_id)
+    form = UpdateUserForm()
+
+    if form.validate_on_submit():
+        user.username=form.username.data,
+        user.password=form.password.data,
+        user.email=form.email.data,
+        user.image_url=form.image_url.data or User.image_url.default.arg,
+        user.header_image_url=form.header_image_url.data or User.header_image_url.default.arg,
+        user.bio=form.bio.data
+            
+        db.session.commit()
+
+        flash(f"Successfully updated {user.username}!", "success")
+        return redirect(f"/users/{user.id}")
+
+    elif IntegrityError:
+        flash("Username already taken", 'danger')
+        return render_template('users/edit.html', form=form)
+
+    else:
+        return render_template('users/edit.html', form=form)
 
 
 @app.route('/users/delete', methods=["POST"])
